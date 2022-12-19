efbc160be50d081e4cf201f7b5146594a9c1429c
Mossaab BOUDCHICHA <mossaab.boudchicha@gmail.com>
2022-12-19 23:21:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Link Sale and MRP
+------------------
+This module makes the link between the Sales and MRP applications. 
+
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Link Sale and MRP',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Sale/Manufacturing',
+    'demo': [],
+    'depends': ['sale','mrp'],
+    'data': [
+        'views/mrp_production_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
