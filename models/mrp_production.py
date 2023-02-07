ccc2863dcfe22006adcc21580ad3600e62ae698f
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-02-07 13:54:24 +0100

    Manufacturing order:Remove the fields <Sale order> and <Customer reference>

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -7,8 +7,5 @@ from odoo.exceptions import UserError
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    sale_id = fields.Many2one('sale.order', 'Sales order', index=True, readonly=True, check_company=True,
-                              states={'draft': [('readonly', False)]})
     partner_id = fields.Many2one('res.partner', string='Customer', readonly=True,
                                  states={'draft': [('readonly', False)]}, store=True, index=True)
-    client_order_ref = fields.Char(string='Customer Reference', related='sale_id.client_order_ref', readonly=True)
